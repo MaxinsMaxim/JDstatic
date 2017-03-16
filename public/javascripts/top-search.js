c9e88deaa738e649e7f489cea8f9198077908a6f
maxin <maxinohnono@hotmail.com>
2017-03-16 09:32:18 +0800

    'update'

--- public/javascripts/top-search.js
+++ public/javascripts/top-search.js
@@ -1,6 +1,7 @@
 var searchInput = document.getElementById('key');
 var shelper = document.getElementById('shelper');
 
+//搜索条焦点事件
 searchInput.onfocus = function () {
     shelper.style.display = 'block';
 }
@@ -8,6 +9,7 @@ searchInput.onblur = function () {
     shelper.style.display = 'none';
 }
 
+//顶部搜索条滚动一定高度淡入
 var searchFix = document.getElementById('search');
 window.onscroll = function () {
     var scrollTop = kirin.win('scrollTop');
@@ -17,3 +19,20 @@ window.onscroll = function () {
         kirin.removeClass(searchFix, 'search-fix');
     }
 }
+
+//左边分类导航
+;(function () {
+    var fs = document.getElementsByClassName('fs')[0];
+    var cate_menu = fs.getElementsByClassName('cate-menu')[0];
+    var cate_menu_item = cate_menu.getElementsByClassName('cate-menu-item');
+    for (var i = 0; i < cate_menu_item.length; i++) {
+        var curItem = cate_menu_item[i];
+        curItem.index = i;
+        curItem.onmouseover = function () {
+            kirin.addClass(this, 'cate_menu_item_on');
+        };
+        curItem.onmouseout = function () {
+            kirin.removeClass(this, 'cate_menu_item_on');
+        };
+    }
+})();
